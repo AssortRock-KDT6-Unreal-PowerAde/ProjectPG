3828b3e21ca19f5073a2cff4da147230b9b65c1b
박경용 <pky941129@gmail.com>
2026-07-30 15:42:00 +0900

    #1 - 프로젝트 생성 - 프로젝트 이름 결정 - 언리얼 프로젝트 파일 생성 - 기본 마네킹 및 애니메이션 스타터 팩 추가

---
+++ Source/ProjectPG/ProjectPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectPG, "ProjectPG" );
